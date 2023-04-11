b0ebe03fd5ad549afda0831093f5b15e3c83f565
akbiv <akbivash@gmail.com>
2023-04-11 23:19:02 +0545

    l

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images:{domains:["bayut-production.s3.eu-central-1.amazonaws.com"]}
+  images: { 
+    domains: ["bayut-production.s3.eu-central-1.amazonaws.com"]
+   },
+   typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
